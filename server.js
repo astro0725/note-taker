2567d32c8310dc6923542f5ed70d94c6a10b4e33
astro <angelica.strong0725@gmail.com>
2024-01-18 17:32:17 -0800

    helper functions for reading & writing to the database file

--- server.js
+++ server.js
@@ -9,4 +9,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+const dbFilePath = path.join(__dirname, 'db/db.json');
+
+function readDbFile() {
+    const notesRaw = fs.readFileSync(dbFilePath);
+    return JSON.parse(notesRaw);
+}
+
+function writeDbFile(notes) {
+    fs.writeFileSync(dbFilePath, JSON.stringify(notes, null, 2));
+}
+
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
