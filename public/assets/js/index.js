5110c70290dcd13121181194c11fd0ce8b0a693a
astro <angelica.strong0725@gmail.com>
2024-01-18 18:03:58 -0800

    added console log to handle note delete function

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -79,6 +79,7 @@ const handleNoteSave = () => {
 
 // Delete the clicked note
 const handleNoteDelete = (e) => {
+  console.log('Attempting to delete a note');
   // Prevents the click listener for the list from being called when the button inside of it is clicked
   e.stopPropagation();
 
